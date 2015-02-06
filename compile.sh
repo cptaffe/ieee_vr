7e767af605521d0de8402aa07b327dbd7531428e
Connor Taffe <cpaynetaffe@gmail.com>
2015-02-06 16:34:44 -0600

    recompile

--- compile.sh
+++ compile.sh
@@ -10,6 +10,19 @@ set -o pipefail
 log_file="log.log"
 work_dir="${HOME}/eac_dev/ieee_vr_vuforia"
 
+log_init() {
+
+  # check args
+  if test "${#}" -ne 1; then
+    echo "incorrect usage of clear_log, exiting."
+    exit 1
+  fi
+
+  echo -n "log for compile.sh on data" &>"${1}"
+  date &>>"${1}"
+  echo "" &>>"${1}"
+}
+
 exit_on_fail() {
 
   # check args
@@ -39,6 +52,9 @@ run_cmd() {
   ${cmd} &>>"${log_file}"
   exit_on_fail "${?}" "${cmd}"
 }
+
+# init log file
+log_init "${log_file}"
 
 # pull and update
 run_cmd "git -C ${work_dir} pull"
